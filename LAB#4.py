9f99d42a52c880259ca528f2f3f7d8dce3f120fd
sobeiter <o_terexov@inbox.ru>
2024-10-11 20:28:52 +0300

    Update LAB#4.py

--- LAB#4.py
+++ LAB#4.py
@@ -1,9 +1,9 @@
 # -- Coding: UTF-8 --
 def main():
-    s = 0
+    q = 0
     n = int(input('Введите количество чисел: '))
     for i in range(n):
         a = int(input(f'Введите число №{i+1}: '))
-        s += a
-    return s
+        q += a
+    return q
 print(main())
